0b5bf6266f91cec54a7a2a1390a287f92b547a3d
Randy Reiza <randyreiza14@gmail.com>
2022-01-31 16:57:58 +0700

    add logic for 7 days back from today, slicing UI (not completed)

---
+++ app/src/main/java/com/example/mycalendar/CalendarFragment.kt
@@ -0,0 +1,277 @@
+package com.example.mycalendar
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.*
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.fragment.app.Fragment
+import com.example.mycalendar.databinding.CalendarDayBinding
+import com.example.mycalendar.databinding.CalendarHeaderBinding
+import com.example.mycalendar.databinding.FragmentCalendarBinding
+import com.google.android.material.snackbar.Snackbar
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import com.kizitonwose.calendarview.utils.yearMonth
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
+
+
+class CalendarFragment : Fragment() {
+
+    private val today = LocalDate.now()
+
+    private var startDate: LocalDate? = null
+    private var endDate: LocalDate? = null
+
+    private val headerDateFormatter = DateTimeFormatter.ofPattern("d MMMM YYYY")
+
+    private val startBackground: GradientDrawable by lazy {
+        requireContext().getDrawableCompat(R.drawable.continuous_selected_bg_middle) as GradientDrawable
+    }
+
+    private val endBackground: GradientDrawable by lazy {
+        requireContext().getDrawableCompat(R.drawable.continuous_selected_bg_middle) as GradientDrawable
+    }
+
+    private lateinit var binding: FragmentCalendarBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCalendarBinding.bind(view)
+
+        // back button
+        binding.backButton.setOnClickListener {
+            Toast.makeText(requireContext(), "Back Button", Toast.LENGTH_SHORT).show()
+        }
+
+        // We set the radius of the continuous selection background drawable dynamically
+        // since the view size is `match parent` hence we cannot determine the appropriate
+        // radius value which would equal half of the view's size beforehand.
+        // set radius {utk mengatur kelengkungan garis hijau (date yg di pick)}
+        binding.calendar.post {
+            val radius = ((binding.calendar.width / 7) / 2).toFloat()
+//            startBackground.setCornerRadius(topLeft = radius, bottomLeft = radius)
+//            endBackground.setCornerRadius(topRight = radius, bottomRight = radius)
+        }
+
+        // Set the First day of week depending on Locale
+        // get and set Days from Locale
+        val daysOfWeek = daysOfWeekFromLocale()
+        binding.legendLayout.root.children.forEachIndexed { i, v ->
+            (v as TextView).apply {
+                text = daysOfWeek[i].getDisplayName(TextStyle.SHORT, Locale.ENGLISH)
+                setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+                setTextColorRes(R.color.grey)
+            }
+        }
+
+        // calendar setup
+        val currentMonth = YearMonth.now()
+        var plusMonth = 0
+        if (today.monthValue != today.minusDays(6).monthValue) plusMonth = 1
+        binding.calendar.setup(currentMonth, currentMonth.plusMonths(plusMonth.toLong()), daysOfWeek.first())
+        binding.calendar.scrollToDate(today.minusDays(14))
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay
+            val binding = CalendarDayBinding.bind(view)
+
+            init {
+                view.setOnClickListener {
+                    if (day.owner == DayOwner.THIS_MONTH &&
+                        (day.date == today ||
+                                (day.date.isAfter(today.minusDays(7)) && day.date.isBefore(today))
+                        )
+                    ) {
+                        val date = day.date
+                        if (startDate != null) {
+                            if (date < startDate || endDate != null) {
+                                startDate = date
+                                endDate = null
+                            } else if (date != startDate) {
+                                endDate = date
+                            }
+                        } else {
+                            startDate = date
+                        }
+                        this@CalendarFragment.binding.calendar.notifyCalendarChanged()
+                        updateDateSummary()
+                    }
+                }
+            }
+        }
+
+        binding.calendar.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                val textView = container.binding.dayText
+                val roundBgView = container.binding.roundBgView
+
+                textView.text = null
+                textView.background = null
+                roundBgView.makeInVisible()
+
+                val startDate = startDate
+                val endDate = endDate
+
+                when (day.owner) {
+                    DayOwner.THIS_MONTH -> {
+                        textView.text = day.day.toString()
+                        if (day.date.isBefore(today.minusDays(6))) {
+                            textView.setTextColorRes(R.color.grey_past)
+                        } else if (day.date.isAfter(today)) {
+                            textView.setTextColorRes(R.color.grey_past)
+                        } else {
+                            when {
+                                startDate == day.date && endDate == null -> {
+                                    textView.setTextColorRes(R.color.white)
+                                    roundBgView.makeVisible()
+                                    roundBgView.setBackgroundResource(R.drawable.continuous_selected_bg_middle)
+                                }
+                                day.date == startDate -> {
+                                    textView.setTextColorRes(R.color.white)
+                                    textView.background = startBackground
+                                }
+                                startDate != null && endDate != null && (day.date > startDate && day.date < endDate) -> {
+                                    textView.setTextColorRes(R.color.white)
+                                    textView.setBackgroundResource(R.drawable.continuous_selected_bg_middle)
+                                }
+                                day.date == endDate -> {
+                                    textView.setTextColorRes(R.color.white)
+                                    textView.background = endBackground
+                                }
+                                day.date == today -> {
+                                    textView.setTextColorRes(R.color.grey)
+                                    roundBgView.makeVisible()
+                                    roundBgView.setBackgroundResource(R.drawable.today_bg)
+                                }
+                                else -> textView.setTextColorRes(R.color.grey)
+                            }
+                        }
+                    }
+//                    // Make the coloured selection background continuous on the invisible in and out dates across various months.
+//                    DayOwner.PREVIOUS_MONTH ->
+//                        if (startDate != null && endDate != null && isInDateBetween(day.date, startDate, endDate)) {
+//                            textView.setBackgroundResource(R.drawable.continuous_selected_bg_middle)
+//                        }
+//                    DayOwner.NEXT_MONTH ->
+//                        if (startDate != null && endDate != null && isOutDateBetween(day.date, startDate, endDate)) {
+//                            textView.setBackgroundResource(R.drawable.continuous_selected_bg_middle)
+//                        }
+                }
+            }
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val textView = CalendarHeaderBinding.bind(view).headerText
+        }
+        binding.calendar.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                val monthTitle = "${month.yearMonth.month.name.toLowerCase().capitalize()} ${month.year}"
+                container.textView.text = monthTitle
+            }
+        }
+
+        // save button
+        binding.submitButton.setOnClickListener click@{
+            val startDate = startDate
+            val endDate = endDate
+            if (startDate != null && endDate != null) {
+                val formatter = DateTimeFormatter.ofPattern("d MMMM yyyy")
+                val text = "Selected: ${formatter.format(startDate)} - ${formatter.format(endDate)}"
+                Snackbar.make(requireView(), text, Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(requireView(), "No selection. Searching all Airbnb listings.", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+            fragmentManager?.popBackStack()
+        }
+
+        updateDateSummary()
+    }
+
+    private fun isInDateBetween(inDate: LocalDate, startDate: LocalDate, endDate: LocalDate): Boolean {
+        if (startDate.yearMonth == endDate.yearMonth) return false
+        if (inDate.yearMonth == startDate.yearMonth) return true
+        val firstDateInThisMonth = inDate.plusMonths(1).yearMonth.atDay(1)
+        return firstDateInThisMonth >= startDate && firstDateInThisMonth <= endDate && startDate != firstDateInThisMonth
+    }
+
+    private fun isOutDateBetween(outDate: LocalDate, startDate: LocalDate, endDate: LocalDate): Boolean {
+        if (startDate.yearMonth == endDate.yearMonth) return false
+        if (outDate.yearMonth == endDate.yearMonth) return true
+        val lastDateInThisMonth = outDate.minusMonths(1).yearMonth.atEndOfMonth()
+        return lastDateInThisMonth >= startDate && lastDateInThisMonth <= endDate && endDate != lastDateInThisMonth
+    }
+
+    private fun updateDateSummary() {
+        binding.startDateText.apply {
+            if (startDate != null) {
+                text = headerDateFormatter.format(startDate)
+                setTextColorRes(R.color.grey)
+            } else {
+                text = getString(R.string.start_date)
+                setTextColor(Color.GRAY)
+            }
+        }
+
+        binding.endDateText.apply {
+            if (endDate != null) {
+                text = headerDateFormatter.format(endDate)
+                setTextColorRes(R.color.grey)
+            } else {
+                text = getString(R.string.end_date)
+                setTextColor(Color.GRAY)
+            }
+        }
+
+        // Enable save button if a range is selected or no date is selected at all, Airbnb style.
+        binding.submitButton.isEnabled = endDate != null || (startDate == null && endDate == null)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val closeIndicator = requireContext().getDrawableCompat(R.drawable.ic_close)?.apply {
+            setColorFilter(requireContext().getColorCompat(R.color.grey), PorterDuff.Mode.SRC_ATOP)
+        }
+        (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(closeIndicator)
+        requireActivity().window.apply {
+            // Update statusbar color to match toolbar color.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                statusBarColor = requireContext().getColorCompat(R.color.white)
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            } else {
+                statusBarColor = Color.GRAY
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        requireActivity().window.apply {
+            // Reset statusbar color.
+            statusBarColor = requireContext().getColorCompat(R.color.colorPrimaryDark)
+            decorView.systemUiVisibility = 0
+        }
+    }
+}
--- app/src/main/java/com/example/mycalendar/MainActivity.kt
+++ app/src/main/java/com/example/mycalendar/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.mycalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.commit
+import com.example.mycalendar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mFragmentManager = supportFragmentManager
+        val mCalendarFragment = CalendarFragment()
+        val fragment = mFragmentManager.findFragmentByTag(CalendarFragment::class.java.simpleName)
+
+        if (fragment !is CalendarFragment) {
+            Log.d("MyCalendar", "Fragment Name :" + CalendarFragment::class.java.simpleName)
+            mFragmentManager.commit {
+                add(R.id.frame_container, mCalendarFragment, CalendarFragment::class.java.simpleName)
+            }
+        }
     }
 }
